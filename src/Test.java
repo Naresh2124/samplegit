58abca944472e3ae274bed14550e4e7c0f7aa221
Naresh2124 <nareshnvr21@gmail.com>
2024-02-07 13:33:07 +0530

    code done

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,10 @@
 
 public class Test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main() {
+		
 
-		System.out.println("test");
+		System.out.println("test3");
 	}
 
 }
